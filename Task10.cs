d0377ba85eb09b49529b1a1ff3a78279f4f96923
YaroslavDubV <yaroslavduboshin@mail.ru>
2022-08-13 20:39:13 +0300

    Task10

---
+++ Task10.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите 3-х значное число:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n >99 && n < 1000 ) {
+    n = n / 10;
+    n = n % 10;
+    Console.WriteLine(n); 
+}
+else if (n < -99 && n > -1000) {
+    n = n / 10;
+    n = n % 10;
+    Console.WriteLine(n); 
+}
+else {
+    Console.WriteLine("Введенное число не является 3-х значным");
+}
